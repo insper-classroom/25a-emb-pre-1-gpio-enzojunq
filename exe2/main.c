c522f4185e57311da3890c7e2356770e60dd2bdb
Enzo <enzopj@al.insper.edu.br>
2025-02-12 15:25:28 -0300

    exercicios feitos

--- exe2/main.c
+++ exe2/main.c
@@ -3,13 +3,19 @@
 #include <stdio.h>
 
 const int BTN_PIN = 26;
+const int BTN_PIN_BLUE = 7;
 
 int main() {
   stdio_init_all();
 
   gpio_init(BTN_PIN);
+  gpio_init(BTN_PIN_BLUE);
+
   gpio_set_dir(BTN_PIN, GPIO_IN);
   gpio_pull_up(BTN_PIN);
+
+  gpio_set_dir(BTN_PIN_BLUE, GPIO_IN);
+  gpio_pull_up(BTN_PIN_BLUE);
 
   while (true) {
     if (!gpio_get(BTN_PIN)) {
@@ -17,5 +23,11 @@ int main() {
       while (!gpio_get(BTN_PIN)) {
       };
     }
+    if (!gpio_get(BTN_PIN_BLUE)) {
+      printf("Botao 2\n");
+      while (!gpio_get(BTN_PIN_BLUE)) {
+      };
+    }
   }
 }
+
--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,60 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-int main() {
+const int BTN_RED = 28;
+const int BTN_GREEN = 26;
+
+const int LED_RED = 4;
+const int LED_GREEN = 6;
+
+int main()
+{
   stdio_init_all();
 
-  while (true) {
+  gpio_init(BTN_RED);
+  gpio_init(BTN_GREEN);
+
+  gpio_set_dir(BTN_RED, GPIO_IN);
+  gpio_pull_up(BTN_RED);
+
+  gpio_set_dir(BTN_GREEN, GPIO_IN);
+  gpio_pull_up(BTN_GREEN);
+
+  gpio_init(LED_RED);
+  gpio_set_dir(LED_RED, GPIO_OUT);
+
+  gpio_init(LED_GREEN);
+  gpio_set_dir(LED_GREEN, GPIO_OUT);
+
+  while (true)
+  {
+    if (!gpio_get(BTN_RED))
+    {
+      if (gpio_get(LED_RED))
+      {
+        gpio_put(LED_RED, 0);
+      }
+      else
+      {
+        gpio_put(LED_RED, 1);
+      }
+      while (!gpio_get(BTN_RED))
+      {
+      };
+    }
+    if (!gpio_get(BTN_GREEN))
+    {
+      if (gpio_get(LED_GREEN))
+      {
+        gpio_put(LED_GREEN, 0);
+      }
+      else
+      {
+        gpio_put(LED_GREEN, 1);
+      }
+      while (!gpio_get(BTN_GREEN))
+      {
+      };
+    }
   }
 }
